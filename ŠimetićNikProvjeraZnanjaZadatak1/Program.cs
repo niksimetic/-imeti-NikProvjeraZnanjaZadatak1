22f3129c285b25d3a42cd6643a7d8c50dfc59e40
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-03-03 12:14:38 +0100

    Add files via upload

---
+++ ŠimetićNikProvjeraZnanjaZadatak1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ŠimetićNikProvjeraZnanjaZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int x = 20;
+            int y=3;
+            double d1;
+            double d2;
+            d1 = x / y;
+            d2 = x % y;
+            Console.WriteLine("D1 je:" + d1); 
+            Console.WriteLine("D2 je:" + d2);
+            Console.ReadKey();
+        }
+    }
+}
